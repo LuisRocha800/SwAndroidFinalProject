d4def97c7743866ce2070c9650bb40795d363b23
Luis Angel Rocha <luis.rocha021231@gmail.com>
2022-05-15 23:13:07 -0500

    first commit

---
+++ app/src/main/java/com/basesdedatos/apperp/Constantes.java
@@ -0,0 +1,6 @@
+package com.basesdedatos.apperp;
+
+public class Constantes {
+
+    private final static String URL_WEB_SERVICE = "https://radiant-journey-98477.herokuapp.com/";
+}
---
+++ app/src/main/java/com/basesdedatos/apperp/principalActivity.java
@@ -0,0 +1,15 @@
+package com.basesdedatos.apperp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class principalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_activity);
+    }
+}
